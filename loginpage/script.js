504f8802cfb86aaa84effea20f8a90b1937c466b
Lucas-AlexNK <Lucasaferreira94@gmail.com>
2024-02-09 08:53:01 -0300

    Adcionando comentários explicativos

--- loginpage/script.js
+++ loginpage/script.js
@@ -1,11 +1,18 @@
+// Obtém a referência ao elemento container pelo seu ID
 const container = document.getElementById('container');
+
+// Obtém a referência aos botões de registro e login pelo seus IDs
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 
+// Adiciona um ouvinte de evento para o clique no botão de registro
 registerBtn.addEventListener('click', () => {
+    // Adiciona a classe "active" ao elemento container
     container.classList.add("active");
 });
 
+// Adiciona um ouvinte de evento para o clique no botão de login
 loginBtn.addEventListener('click', () => {
+    // Remove a classe "active" do elemento container
     container.classList.remove("active");
 });
